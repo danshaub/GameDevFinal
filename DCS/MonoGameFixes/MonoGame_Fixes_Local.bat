2565027575e286a7b78728e2c98a9b0f1f6e7983
Haub <haub@chapman.edu>
2019-11-19 12:47:53 -0800

    worked more on fire pit and added DCS folder

---
+++ DCS/MonoGameFixes/MonoGame_Fixes_Local.bat
@@ -0,0 +1,3 @@
+if not exist "C:\Users\%username%\Documents\Visual Studio 2017\Templates\ProjectTemplates\Visual C#\MonoGame\" (
+mkdir "C:\Users\%username%\Documents\Visual Studio 2017\Templates\ProjectTemplates\Visual C#\MonoGame\" )
+xcopy "C:\DCS\MonoGameFixes\*.zip" "C:\Users\%username%\Documents\Visual Studio 2017\Templates\ProjectTemplates\Visual C#\MonoGame\"
---
+++ DCS/UnityFixes/activate2019.bat
@@ -0,0 +1,3 @@
+if exist "C:\ProgramData\Unity\Unity_lic.ulf" del "c:\programdata\unity\unity_lic.ulf"
+
+"%ProgramFiles%\Unity\Editor\Unity.exe" -quit -batchmode -serial E3-BJCT-8WQ6-X6UX-9E27-UTXV -username "istsoftware@chapman.edu" -password "VAnJZX8cORJFrS36dqWPPH1fcS"
